1e6d59b453a7088934407a6db471d01c90501ef7
Vikas <bairwavikas5@gmail.com>
2022-01-31 12:36:59 +0530

    added env

--- api/app.js
+++ api/app.js
@@ -1,15 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 var cors = require('cors')
 app.use(cors())
 const fetch = require('node-fetch');
+const port = process.env.PORT;
+const url = process.env.URL;
 app.get('/cors', async(request, response) => {
-    const api_url = 'https://bolkar-club.s3.ap-south-1.amazonaws.com/task/tutors.json';
+    const api_url = url;
     const data = await fetch(api_url);
     const r = await data.json();
-    console.log(r);
     response.send(r);
     })
-app.listen(8080, () => {
-    console.log('listening on port 8080')
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
 })
